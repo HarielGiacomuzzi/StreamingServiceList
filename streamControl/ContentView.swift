849dfe4763d433170175e76e31401915e2fa1f52
harielGiacomuzzi <hariel.dias@acad.pucrs.br>
2020-04-11 20:36:54 -0300

    Initial Commit

---
+++ streamControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  streamControl
+//
+//  Created by Hariel Giacomuzzi on 11/04/20.
+//  Copyright © 2020 Hariel Giacomuzzi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
